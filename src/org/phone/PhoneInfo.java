9b6f6a05722c0b3a7a225441bf36af6309970707
MohanaJoshika <shakthimona@gmail.com>
2022-12-13 11:02:06 +0530

    Demo

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,34 @@
+package org.phone;
+
+public class PhoneInfo {
+
+	private void phoneName() {
+		// TODO Auto-generated method stub
+
+	}
+	private void phoneMieiNum() {
+		// TODO Auto-generated method stub
+
+	}
+	private void camera() {
+		// TODO Auto-generated method stub
+
+	}
+	private void storage() {
+		// TODO Auto-generated method stub
+
+	}
+	private void osName() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void main(String[] args) {
+		PhoneInfo pi=new PhoneInfo();
+		pi.phoneName();
+		pi.phoneMieiNum();
+		pi.camera();
+		pi.storage();
+		pi.osName();
+	}
+}
